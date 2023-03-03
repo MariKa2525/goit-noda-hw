02cb2635a755300e2dcb8329e4b82f6fad3b317b
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2023-03-03 14:30:51 +0200

    add

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 const functions = require('./contacts')
-
-
-// functions.listContacts();
-// functions.getContactById('5');
-// functions.removeContact('7');
-// functions.addContact('Anna Anna', 'email.@gmail.com', '(888) 212-212');
-
 const argv = require('yargs').argv
 
 async function invokeAction({ action, id, name, email, phone }) {
@@ -35,32 +28,3 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv)
-
-// // index.js
-// const argv = require('yargs').argv;
-
-// // TODO: рефакторить
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case 'list':
-//       console.log('list')
-//       break;
-
-//     case 'get':
-//        console.log('id',id)
-//       break;
-
-//     case 'add':
-//      console.log( 'name email phone', name, email, phone)
-//       break;
-
-//     case 'remove':
-//       console.log('id',id)
-//       break;
-
-//     default:
-//       console.warn('\x1B[31m Unknown action type!');
-//   }
-// }
-
-// invokeAction(argv);
